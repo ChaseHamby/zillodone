d97469eefb4120c33e34e9eb81755c9607eaf0ca
Chase Hamby <c.chase.hamby@gmail.com>
2018-12-17 15:41:08 -0600

    positioned everything

--- src/components/ListingForm/listingform.js
+++ src/components/ListingForm/listingform.js
@@ -12,25 +12,8 @@ import './listingform.scss';
 class ListingForm extends React.Component {
   render() {
     return (
-      <div className="listingForm">
+      <div className="listingForm col">
         <h2>Listing Form</h2>
-      <Form>
-        <FormGroup>
-          <Label for="exampleEmail">Email</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="examplePassword">Password</Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-        </FormGroup>
-        <FormGroup check>
-          <Label check>
-            <Input type="checkbox" />{' '}
-            Check me out
-          </Label>
-        </FormGroup>
-        <Button>Submit</Button>
-      </Form>
       </div>
     );
   }
