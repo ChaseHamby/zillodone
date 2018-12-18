4bb43c1e35e9c2ed1178031df11f64c4f939f0f7
Chase Hamby <c.chase.hamby@gmail.com>
2018-12-17 21:29:32 -0600

    clean

--- src/App/App.js
+++ src/App/App.js
@@ -14,6 +14,7 @@ import authRequests from '../helpers/data/authRequests';
 class App extends Component {
   state = {
     authed: false,
+    github_username: '',
     listings: [],
   }
 
@@ -42,8 +43,8 @@ class App extends Component {
     this.removeListener();
   }
 
-  isAuthenticated = () => {
-    this.setState({ authed: true });
+  isAuthenticated = (username) => {
+    this.setState({ authed: true, github_username: username });
   }
 
   deleteOne = (listingId) => {
@@ -60,7 +61,7 @@ class App extends Component {
   render() {
     const logoutClickEvent = () => {
       authRequests.logoutUser();
-      this.setState({ authed: false });
+      this.setState({ authed: false, github_username: '' });
     };
 
     if (!this.state.authed) {
