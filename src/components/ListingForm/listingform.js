261c32a6c94d4276c8a496a8ff5a95caf56fe8e9
Chase Hamby <c.chase.hamby@gmail.com>
2018-12-18 19:18:40 -0600

    added listing form

--- src/components/ListingForm/listingform.js
+++ src/components/ListingForm/listingform.js
@@ -12,9 +12,22 @@ import './listingform.scss';
 class ListingForm extends React.Component {
   render() {
     return (
-      <div className="listingForm col">
-        <h2>Listing Form</h2>
-      </div>
+      <div className="listing-form col">
+        <h2>Add New Listing: </h2>
+        <form>
+          <div className="form-group">
+            <label for="address">Address: </label>
+            <input 
+              type="text" 
+              className="form-control" 
+              id="address" 
+              aria-describedby="addressHelp" 
+              placeholder="123 Main Street Nashville, TN 37069" 
+            />
+          </div>
+          <button className="btn btn-danger">Save Listing</button>
+      </form>
+    </div>
     );
   }
 }
